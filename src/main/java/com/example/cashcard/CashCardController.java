9169d5424642c0aa35c2ae1cc7ef90e9888b1bac
inigorajv <inigoraj.vincent@zemosolabs.com>
2024-03-19 12:46:15 +0530

    h2 db and repository setup

--- src/main/java/com/example/cashcard/CashCardController.java
+++ src/main/java/com/example/cashcard/CashCardController.java
@@ -1,21 +1,31 @@
 package com.example.cashcard;
 
 import com.example.cashcard.entity.CashCard;
+import com.example.cashcard.repository.CashCardRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("v1/cashcards")
 public class CashCardController {
 
+    private CashCardRepository cashCardRepository;
+
+    CashCardController(CashCardRepository cashCardRepository){
+        this.cashCardRepository = cashCardRepository;
+
+    }
+
     @GetMapping("/{id}")
     ResponseEntity<CashCard> getCashCardById(@PathVariable Long id){
-        if(id.equals(99L)){
-            CashCard cashCard = new CashCard(99L, 123.45);
-            return ResponseEntity.ok(cashCard);
+        Optional<CashCard> cashCard = cashCardRepository.findById(id);
+        if(cashCard.isPresent()){
+            return ResponseEntity.ok(cashCard.get());
         }else{
             return ResponseEntity.notFound().build();
         }
---
+++ src/main/java/com/example/cashcard/repository/CashCardRepository.java
@@ -0,0 +1,8 @@
+package com.example.cashcard.repository;
+
+import com.example.cashcard.entity.CashCard;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CashCardRepository extends CrudRepository<CashCard, Long> {
+
+}
